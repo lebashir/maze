a41347665130f722df2e454e6839403a69041ea5
lebashir <bashir.abihabib@gmail.com>
2021-05-05 13:26:46 -0500

    generated maze and setup the game

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cells = 3;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells;
+
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
     element: document.body,
@@ -22,10 +25,10 @@ Runner.run(Runner.create(), engine);
 
 // Walls
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
 ];
 
 World.add(world, walls)
@@ -104,10 +107,102 @@ const stepThroughCell = (row, column) => {
         } else if (direction ==='down') {
             horizontals[row][column] = true;
         }
+        //visit that next cell
         stepThroughCell(nextRow, nextColumn)
     }
-    //visit that next cell
+    
 };
 
 stepThroughCell(1, 1)
 
+
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+        
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength / 2,
+            rowIndex * unitLength + unitLength,
+            unitLength,
+            1,
+            {
+                label: 'wall',
+                isStatic: true
+            } 
+        );
+        World.add(world, wall)
+    })
+});
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if(open) {
+            return;
+        }
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength / 2,
+            1,
+            unitLength,
+            {
+                label: 'wall',
+                isStatic: true
+            }
+        );
+        World.add(world, wall)
+    })
+})
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * 0.7,
+    unitLength * 0.7,
+    {
+        isStatic: true,
+        label: 'goal'
+    }
+
+);
+World.add(world, goal);
+
+const ball = Bodies.circle(
+    unitLength / 2,
+    unitLength / 2,
+    unitLength / 3,
+    {
+        label: 'ball'
+    }
+)
+World.add(world, ball);
+
+document.addEventListener('keydown', event => {
+    const {x, y} = ball.velocity;
+    
+    if (event.key === 'w' || event.key === 'ArrowUp') {
+        Body.setVelocity(ball, {x, y: y - 5})
+    }
+    if (event.key === 'd' || event.key === 'ArrowRight') {
+        Body.setVelocity(ball, {x: x + 5, y})
+    }
+    if (event.key === 's' || event.key === 'ArrowDown') {
+        Body.setVelocity(ball, {x, y: y + 5})
+    }
+    if (event.key === 'a' || event.key === 'ArrowLeft') {
+        Body.setVelocity(ball, {x: x - 5, y})
+    }
+});
+
+// Win Condition
+
+Events.on(engine, 'collisionStart', event => {
+    event.pairs.forEach(collision => {
+        const labels = ['ball', 'goal'];
+        if (
+            labels.includes(collision.bodyA.label) && 
+            labels.includes(collision.bodyB.label)
+        ) {
+            world.gravity.y = 1;
+        }
+    })
+});
